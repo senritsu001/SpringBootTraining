98527039f817ea8cb6beea1865404223be14fca9
seirinsou <scl0905myc@gmail.com>
2021-04-25 17:28:58 +0900

    sc1

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,21 +1,25 @@
 package com.example.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.demo.model.User;
 
 @Controller
+@Validated
 public class HomeController {
 
     @GetMapping("/form")
-    private String readForm(@ModelAttribute User user) {
-        return "form";
+    private String login(@ModelAttribute User user) {
+        return "login";
     }
 
     @PostMapping("/form")
-    private String confirm(@ModelAttribute User user) {
-        return "confirm";
+    private String top(@Valid @ModelAttribute User user) {
+        return "top";
     }
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,11 +1,25 @@
 package com.example.demo.model;
 
-import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
-@Data
 public class User {
-    private String name;
+	@Email
     private String email;
-    private Integer age;
+	@NotNull
+    private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
     
 }
